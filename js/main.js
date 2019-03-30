363abb96a5b4c3ab3b6f647e0459cfa7dd71197f
Jeffrey Lowy <jeffreylowy@gmail.com>
2019-03-30 13:53:50 -0700

    Clean-up JS linter nits

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ var restaurants,
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
+// @ts-ignore
 document.addEventListener("DOMContentLoaded", event => {
   initMap(); // added
   fetchNeighborhoods();
@@ -16,12 +17,13 @@ document.addEventListener("DOMContentLoaded", event => {
 /**
  * Fetch all neighborhoods and set their HTML.
  */
-fetchNeighborhoods = () => {
+const fetchNeighborhoods = () => {
   DBHelper.fetchNeighborhoods((error, neighborhoods) => {
     if (error) {
       // Got an error
       console.error(error);
     } else {
+      // @ts-ignore
       self.neighborhoods = neighborhoods;
       fillNeighborhoodsHTML();
     }
@@ -31,7 +33,8 @@ fetchNeighborhoods = () => {
 /**
  * Set neighborhoods HTML.
  */
-fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
+// @ts-ignore
+const fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
   const select = document.getElementById("neighborhoods-select");
   neighborhoods.forEach(neighborhood => {
     const option = document.createElement("option");
@@ -44,12 +47,13 @@ fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
 /**
  * Fetch all cuisines and set their HTML.
  */
-fetchCuisines = () => {
+const fetchCuisines = () => {
   DBHelper.fetchCuisines((error, cuisines) => {
     if (error) {
       // Got an error!
       console.error(error);
     } else {
+      // @ts-ignore
       self.cuisines = cuisines;
       fillCuisinesHTML();
     }
@@ -59,7 +63,8 @@ fetchCuisines = () => {
 /**
  * Set cuisines HTML.
  */
-fillCuisinesHTML = (cuisines = self.cuisines) => {
+// @ts-ignore
+const fillCuisinesHTML = (cuisines = self.cuisines) => {
   const select = document.getElementById("cuisines-select");
 
   cuisines.forEach(cuisine => {
@@ -73,15 +78,18 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 /**
  * Initialize leaflet map, called from HTML.
  */
-initMap = () => {
+const initMap = () => {
+  // @ts-ignore
   self.newMap = L.map("map", {
     center: [40.722216, -73.987501],
     zoom: 12,
     scrollWheelZoom: false
   });
+  // @ts-ignore
   L.tileLayer(
     "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}",
     {
+      // @ts-ignore
       mapboxToken: mapboxApiKey,
       maxZoom: 18,
       attribution:
@@ -110,11 +118,13 @@ initMap = () => {
 /**
  * Update page and map for current restaurants.
  */
-updateRestaurants = () => {
+const updateRestaurants = () => {
   const cSelect = document.getElementById("cuisines-select");
   const nSelect = document.getElementById("neighborhoods-select");
 
+  // @ts-ignore
   const cIndex = cSelect.selectedIndex;
+  // @ts-ignore
   const nIndex = nSelect.selectedIndex;
 
   const cuisine = cSelect[cIndex].value;
@@ -128,7 +138,9 @@ updateRestaurants = () => {
         // Got an error!
         console.error(error);
       } else {
+        // @ts-ignore
         resetRestaurants(restaurants);
+        // @ts-ignore
         fillRestaurantsHTML();
       }
     }
@@ -138,34 +150,44 @@ updateRestaurants = () => {
 /**
  * Clear current restaurants, their HTML and remove their map markers.
  */
+// @ts-ignore
 resetRestaurants = restaurants => {
   // Remove all restaurants
+  // @ts-ignore
   self.restaurants = [];
   const ul = document.getElementById("restaurants-list");
   ul.innerHTML = "";
 
   // Remove all map markers
+  // @ts-ignore
   if (self.markers) {
+    // @ts-ignore
     self.markers.forEach(marker => marker.remove());
   }
+  // @ts-ignore
   self.markers = [];
+  // @ts-ignore
   self.restaurants = restaurants;
 };
 
 /**
  * Create all restaurants HTML and add them to the webpage.
  */
+// @ts-ignore
 fillRestaurantsHTML = (restaurants = self.restaurants) => {
   const ul = document.getElementById("restaurants-list");
   restaurants.forEach(restaurant => {
+    // @ts-ignore
     ul.append(createRestaurantHTML(restaurant));
   });
+  // @ts-ignore
   addMarkersToMap();
 };
 
 /**
  * Create restaurant HTML.
  */
+// @ts-ignore
 createRestaurantHTML = restaurant => {
   const li = document.createElement("li");
 
@@ -197,14 +219,17 @@ createRestaurantHTML = restaurant => {
 /**
  * Add markers for current restaurants to the map.
  */
+// @ts-ignore
 addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
+    // @ts-ignore
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
     marker.on("click", onClick);
     function onClick() {
       window.location.href = marker.options.url;
     }
+    // @ts-ignore
     self.markers.push(marker);
   });
 };
